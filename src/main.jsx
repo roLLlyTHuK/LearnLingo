a88d3c70ac7ad45ec4c1deedf11ca61938dc9f23
roLLlyTHuK <ircvipua@gmail.com>
2024-06-26 14:18:12 +0300

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './firebase.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/LearnLingo/">
+    <BrowserRouter basename="/LearnLingo">
       <AuthProvider>
         <App />
       </AuthProvider>
