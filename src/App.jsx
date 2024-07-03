1af22c3f7d76763ccadcfd270fc9c8d2e0d3f9cb
roLLlyTHuK <71319633+roLLlyTHuK@users.noreply.github.com>
2024-07-03 13:47:14 +0300

    refactor project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import { AppWrapper } from './App.styled';
-import SharedLayout from '../SharedLayout/SharedLayout';
-import HomePage from '../../pages/HomePage/HomePage';
-import TeachersPage from '../../pages/TeachersPage/TeachersPage';
-import PrivateRoute from '../../routes/PrivateRoute';
-import FavoritesPage from '../../pages/FavoritesPage/FavoritesPage';
-import ErrorPage from '../../pages/ErrorPage/ErrorPage';
+import SharedLayout from './components/SharedLayout/SharedLayout';
+import HomePage from './pages/HomePage/HomePage';
+import TeachersPage from './pages/TeachersPage/TeachersPage';
+import PrivateRoute from './routes/PrivateRoute';
+import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App.jsx';
+import App from './App.jsx';
 import GlobalStyles from './rootStyles.js';
 import { AuthProvider } from './context/AuthContext.jsx';
 import './firebase.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/learn-lingo/">
+    <BrowserRouter basename="/">
       <AuthProvider>
         <GlobalStyles />
         <App />
